491bd1e8dcf9ab73c3b270c67b57bc3342d853e7
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-09 17:39:51 -0300

    Update conway::tick so that cell_should_die_when_it_is_alone

--- src/conway.rs
+++ src/conway.rs
@@ -1,7 +1,8 @@
 use sparse_bin_mat::SparseBinMat;
 
 pub fn tick(grid: &SparseBinMat) -> SparseBinMat{
-    grid.clone()
+    let n = grid.number_of_rows();
+    SparseBinMat::zeros(n,n)
 }
 
 
@@ -10,10 +11,21 @@ mod tests {
     use crate::conway;
     use sparse_bin_mat::SparseBinMat;
     
+    fn get_empty_grid(n: usize) -> SparseBinMat {
+        SparseBinMat::zeros(n,n)
+    }
+
     #[test]
-    fn conway_an_empty_grid_should_stay_empty_after_tick() {
-        let grid = SparseBinMat::new(1,vec![]);
+    fn empty_grid_should_stay_empty_after_tick() {
+        let grid = get_empty_grid(1);
         assert_eq!(grid,conway::tick(&grid));
+    }
+
+    #[test]
+    fn cell_should_die_when_it_is_alone() {
+        let grid_one_cell = SparseBinMat::new(1,vec![vec![0]]);
+        let empty_grid = get_empty_grid(1);
+        assert_eq!(empty_grid,conway::tick(&grid_one_cell));
     }
 }
 
