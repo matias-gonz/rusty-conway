3525ff2c58d0db8080e247c31a2c843b00c876f7
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-10 19:46:39 -0300

    Add conway::is_alive()

--- src/conway.rs
+++ src/conway.rs
@@ -1,14 +1,30 @@
 use sparse_bin_mat::SparseBinMat;
+use sparse_bin_mat::BinNum;
 
 pub fn tick(grid: &SparseBinMat) -> SparseBinMat{
     let n = grid.number_of_rows();
-    SparseBinMat::zeros(n,n)
+    let mut new_grid = SparseBinMat::zeros(n,n);
+
+    for i in 0..n {
+        for j in 0..n {
+            let cell = grid.get(i, j).unwrap();
+            match is_alive(cell) {
+                true => {}
+                false => {}
+            }
+        }
+    }
+    new_grid
+}
+
+fn is_alive(cell: BinNum) -> bool {
+    cell.is_one()
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::conway;
+    use crate::conway::{self, is_alive};
     use sparse_bin_mat::SparseBinMat;
     
     fn get_empty_grid(n: usize) -> SparseBinMat {
@@ -33,6 +49,12 @@ mod tests {
         let grid_one_cell = SparseBinMat::new(2,vec![vec![0],vec![0]]);
         let empty_grid = get_empty_grid(2);
         assert_eq!(empty_grid,conway::tick(&grid_one_cell));
+    }
+
+    #[test]
+    fn cell_is_alive_when_it_is_one() {
+        let cell = SparseBinMat::new(1,vec![vec![0]]).get(0,0).unwrap();
+        assert!(is_alive(cell));
     }
 }
 
