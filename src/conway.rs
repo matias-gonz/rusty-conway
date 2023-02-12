6d7f21da016ed254f0c2f8fd9b6c0d6c16ee0086
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-12 10:22:18 -0300

    Update conway::tick

--- src/conway.rs
+++ src/conway.rs
@@ -7,38 +7,53 @@ pub fn tick(grid: &SparseBinMat) -> SparseBinMat{
 
     for i in 0..n {
         for j in 0..n {
-            let cell = grid.get(i, j).unwrap();
-            match cell_is_alive(cell) {
-                true => {}
-                false => {}
-            }
+            let new_cell = get_new_cell(i,j,grid);
+            new_grid = new_grid.emplace_at(new_cell, i, j);
         }
     }
     new_grid
+}
+
+fn get_new_cell(i: usize, j: usize, grid: &SparseBinMat) -> u8 {
+    let cell: BinNum = grid.get(i, j).unwrap();
+    let mut new_cell = 0;
+    match cell_is_alive(cell) {
+        true => {
+            if cell_lives_onto_next_generation(i, j, grid){
+                new_cell = 1;
+            }
+        }
+        false => {
+            if cell_can_reproduce(i, j, grid){
+                new_cell = 1;
+            }
+        }
+    }
+    new_cell
 }
 
 fn cell_is_alive(cell: BinNum) -> bool {
     cell.is_one()
 }
 
-fn cell_is_underpopulated(x: usize, y: usize, grid: SparseBinMat) -> bool {
+fn cell_is_underpopulated(x: usize, y: usize, grid: &SparseBinMat) -> bool {
     cell_alive_neighbour_count(x, y, grid) < 2
 }
 
-fn cell_is_overpopulated(x: usize, y: usize, grid: SparseBinMat) -> bool {
+fn cell_is_overpopulated(x: usize, y: usize, grid: &SparseBinMat) -> bool {
     cell_alive_neighbour_count(x, y, grid) > 3
 }
 
-fn cell_can_reproduce(x: usize, y: usize, grid: SparseBinMat) -> bool {
+fn cell_can_reproduce(x: usize, y: usize, grid: &SparseBinMat) -> bool {
     cell_alive_neighbour_count(x, y, grid) == 3
 }
 
-fn cell_lives_onto_next_generation(x: usize, y: usize, grid: SparseBinMat) -> bool {
+fn cell_lives_onto_next_generation(x: usize, y: usize, grid: &SparseBinMat) -> bool {
     let count = cell_alive_neighbour_count(x, y, grid);
     count == 2 || count == 3
 }
 
-fn cell_alive_neighbour_count(x: usize, y: usize, grid: SparseBinMat) -> usize{
+fn cell_alive_neighbour_count(x: usize, y: usize, grid: &SparseBinMat) -> usize{
     let mut count = 0;
     for i in 0 ..= 2{
         for j in 0 ..= 2{
@@ -81,9 +96,15 @@ mod tests {
 
     #[test]
     fn two_cells_should_die() {
-        let grid_one_cell = SparseBinMat::new(2,vec![vec![0],vec![0]]);
+        let grid_two_cell = SparseBinMat::new(2,vec![vec![0],vec![0]]);
         let empty_grid = get_empty_grid(2);
-        assert_eq!(empty_grid,tick(&grid_one_cell));
+        assert_eq!(empty_grid,tick(&grid_two_cell));
+    }
+
+    #[test]
+    fn four_cell_square_should_live() {
+        let grid = SparseBinMat::new(2,vec![vec![0,1],vec![0,1]]);
+        assert_eq!(grid,tick(&grid));
     }
 
     #[test]
@@ -95,91 +116,91 @@ mod tests {
     #[test]
     fn cell_has_no_alive_neighbours_when_it_is_the_only_in_the_grid() {
         let grid = SparseBinMat::new(1,vec![vec![0]]);
-        assert_eq!(cell_alive_neighbour_count(0,0,grid), 0);
+        assert_eq!(cell_alive_neighbour_count(0,0,&grid), 0);
     }
 
     #[test]
     fn cell_has_no_alive_neighbours_when_it_is_far_from_others() {
         let rows = vec![vec![0,1,2,3,4],vec![0,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert_eq!(cell_alive_neighbour_count(2,2,grid), 0);
+        assert_eq!(cell_alive_neighbour_count(2,2,&grid), 0);
     }
 
     #[test]
     fn cell_has_one_alive_neighbour() {
         let rows = vec![vec![1],vec![1],vec![]];
         let grid = SparseBinMat::new(3,rows);
-        assert_eq!(cell_alive_neighbour_count(1,1,grid), 1);
+        assert_eq!(cell_alive_neighbour_count(1,1,&grid), 1);
     }
     
     #[test]
     fn cell_has_two_alive_neighbours() {
         let rows = vec![vec![0,1,2,3,4],vec![0,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert_eq!(cell_alive_neighbour_count(0,0,grid), 2);
+        assert_eq!(cell_alive_neighbour_count(0,0,&grid), 2);
     }
 
     #[test]
     fn cell_has_six_alive_neighbours() {
         let rows = vec![vec![0,1,2,3,4],vec![0,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert_eq!(cell_alive_neighbour_count(1,1,grid), 6);
+        assert_eq!(cell_alive_neighbour_count(1,1,&grid), 6);
     }
 
     #[test]
     fn cell_is_underpopulated_when_it_has_one_neighbour() {
         let rows = vec![vec![1],vec![1],vec![]];
         let grid = SparseBinMat::new(3,rows);
-        assert!(cell_is_underpopulated(1,1,grid));
+        assert!(cell_is_underpopulated(1,1,&grid));
     }
 
     #[test]
     fn cell_is_underpopulated_when_it_has_no_neighbours() {
         let rows = vec![vec![0,1,2,3,4],vec![0,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert!(cell_is_underpopulated(2,2,grid));
+        assert!(cell_is_underpopulated(2,2,&grid));
     }
 
     #[test]
     fn cell_is_not_underpopulated_when_it_has_two_neighbours() {
         let rows = vec![vec![0,1,2,3,4],vec![0,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert!(!cell_is_underpopulated(0,0,grid));
+        assert!(!cell_is_underpopulated(0,0,&grid));
     }
 
     #[test]
     fn cell_is_not_overpopulated_when_it_has_two_alive_neighbours() {
         let rows = vec![vec![0,1,2,3,4],vec![0,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert!(!cell_is_overpopulated(0,0,grid));
+        assert!(!cell_is_overpopulated(0,0,&grid));
     }
 
     #[test]
     fn cell_is_overpopulated_when_it_has_six_alive_neighbours() {
         let rows = vec![vec![0,1,2,3,4],vec![0,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert!(cell_is_overpopulated(1,1,grid));
+        assert!(cell_is_overpopulated(1,1,&grid));
     }
 
     #[test]
     fn cell_can_reproduce_when_it_has_three_alive_neighbours() {
         let rows = vec![vec![0,1,2,3,4],vec![0,1,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert!(cell_can_reproduce(0,0,grid));
+        assert!(cell_can_reproduce(0,0,&grid));
     }
 
     #[test]
     fn cell_lives_when_it_has_three_alive_neighbours() {
         let rows = vec![vec![0,1,2,3,4],vec![0,1,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert!(cell_lives_onto_next_generation(0,0,grid));
+        assert!(cell_lives_onto_next_generation(0,0,&grid));
     }
 
     #[test]
     fn cell_doesnt_live_when_it_has_six_alive_neighbours() {
         let rows = vec![vec![0,1,2,3,4],vec![0,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
         let grid = SparseBinMat::new(5,rows);
-        assert!(!cell_lives_onto_next_generation(1,1,grid));
+        assert!(!cell_lives_onto_next_generation(1,1,&grid));
     }
 }
 
