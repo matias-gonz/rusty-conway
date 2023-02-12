65e4c85df5ac3863edf83e97985b4e8ebdc40f4d
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-12 12:34:07 -0300

    Add random grid generator

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,27 @@ pub mod conway;
 
 use std::{thread::{sleep}, time::Duration};
 
+use rand::*;
+
 use sparse_bin_mat::SparseBinMat;
 
 pub mod view;
 
 #[macroquad::main("Rusty Conway")]
 async fn main() {
-    let n = 5;
-    
-    let rows = vec![vec![0,1,2,3,4],vec![0,4],vec![0,2,4],vec![0,4],vec![0,1,2,3,4]];
-    let mut grid = SparseBinMat::new(n,rows);
+    let n = 25;
+    let mut grid = SparseBinMat::zeros(n,n);
+    let mut rng = rand::thread_rng();
+
+    for i in 0..n {
+        for j in 0..n {
+            grid = grid.emplace_at(rng.gen_range(0..=1), i, j);
+        }
+    }
 
     loop {
         view::draw(n, &grid).await;
         grid = conway::tick(&grid);
-        sleep(Duration::from_millis(200));
+        sleep(Duration::from_millis(50));
     }
 }
